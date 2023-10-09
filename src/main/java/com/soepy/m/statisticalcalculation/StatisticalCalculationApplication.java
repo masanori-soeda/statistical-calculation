9d82a7ad06c827031699af1d227178723a3e16f3
soepy <m.soepy@gmail.com>
2023-10-09 11:54:24 +0900

    init commit

---
+++ src/main/java/com/soepy/m/statisticalcalculation/StatisticalCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.soepy.m.statisticalcalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticalCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticalCalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soepy/m/statisticalcalculation/StatisticalCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soepy.m.statisticalcalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticalCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
